619f5953a55e8831d981d0c0384c06b5dffb0cc0
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-27 10:38:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     t = Time.now
-    if t >
+    if t.hour >
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
+    end 
     resp.finish
   end
 end
